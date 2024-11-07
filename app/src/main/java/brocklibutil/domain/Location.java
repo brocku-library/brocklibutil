efcb614bbf08d9bcd29acdbadb3bfd8c4773ac0d
Pranjal Chakraborty <pchakraborty@brocku.ca>
2024-11-07 12:40:23 -0500

    Added QR codes for makerspace labs

--- app/src/main/java/brocklibutil/domain/Location.java
+++ app/src/main/java/brocklibutil/domain/Location.java
@@ -3,20 +3,22 @@ package brocklibutil.domain;
 public enum Location {
 
     A("Classroom A",
-            "https://outlook.office365.com/owa/calendar/5f23feb742c74d13b146423a721005b1@brocku.ca/7ad69b79396b432b9a8efc002f690e6014281070077971958054/calendar.ics"),
+            "https://outlook.office365.com/owa/calendar/5f23feb742c74d13b146423a721005b1@brocku.ca/7ad69b79396b432b9a8efc002f690e6014281070077971958054/calendar.ics", null),
     B("Classroom B",
-            "https://outlook.office365.com/owa/calendar/03c6fd88fe3e4748ae2143b085315d30@brocku.ca/583a9579149143cd9bb30961437149f710974516195299954123/calendar.ics"),
+            "https://outlook.office365.com/owa/calendar/03c6fd88fe3e4748ae2143b085315d30@brocku.ca/583a9579149143cd9bb30961437149f710974516195299954123/calendar.ics", null),
     AUDIO("Audio Lab",
-            "https://outlook.office365.com/owa/calendar/f6fd2446051242b1a5aff92fda0a0f4b@brocku.onmicrosoft.com/9f1823f7f95446bf9177c2b9e032537512783951697716062366/calendar.ics"),
+            "https://outlook.office365.com/owa/calendar/f6fd2446051242b1a5aff92fda0a0f4b@brocku.onmicrosoft.com/9f1823f7f95446bf9177c2b9e032537512783951697716062366/calendar.ics", "/icons/audio-lab-qr-code.svg"),
     VIDEO("Video Lab",
-            "https://outlook.office365.com/owa/calendar/31eed3a812354c8aa870d25362070bcf@brocku.onmicrosoft.com/0cebe67d1b394084ae46a1c158cc678916855064932815811391/calendar.ics");
+            "https://outlook.office365.com/owa/calendar/31eed3a812354c8aa870d25362070bcf@brocku.onmicrosoft.com/0cebe67d1b394084ae46a1c158cc678916855064932815811391/calendar.ics", "/icons/video-lab-qr-code.svg");
 
     String title;
     String URI;
+    String qrCodeURI;
 
-    Location(String title, String URI) {
+    Location(String title, String URI, String qrCodeURI) {
         this.title = title;
         this.URI = URI;
+        this.qrCodeURI = qrCodeURI;
     }
 
     public String getTitle() {
@@ -25,5 +27,9 @@ public enum Location {
 
     public String getURI() {
         return this.URI;
+    }
+
+    public String getQrCodeURI() {
+        return qrCodeURI;
     }
 }
